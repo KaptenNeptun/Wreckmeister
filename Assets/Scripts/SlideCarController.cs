8ccc68f832eeb350622f4f5978374c1520ff4b7f
J. Nat. L. Ronson <KaptenNeptun@users.noreply.github.com>
2018-10-21 16:09:26 +0200

    Tweaked VehicleBehaviour

--- Assets/Scripts/SlideCarController.cs
+++ Assets/Scripts/SlideCarController.cs
@@ -11,16 +11,19 @@ public class SlideCarController : MonoBehaviour
     public WheelCollider frontRightWheel, frontLeftWheel; //Front wheelcolliders
     public WheelCollider backRightWheel, backLeftWheel; //Back wheelcolliders
 
+    public GameObject motorPosition;
     public Transform frontRightWheelT, frontLeftWheelT; //Front wheel transform
     public Transform backRightWheelT, backLeftWheelT; //Back wheel transform
 
     public float maxSteerAngle = 30;
-    public float motorForce = 50;
+    public float motorForce = 50f;
     Rigidbody m_Rigidbody;
 
     private void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+
+
     }
 
     public void GetInput()
@@ -38,27 +41,43 @@ public class SlideCarController : MonoBehaviour
     }
     private void Accelerate()
     {
-        if (m_VerticalInput != 0)
+        //m_Rigidbody.AddForceAtPosition(m_Rigidbody.transform.forward * m_VerticalInput * motorForce, motorPosition.transform.position, ForceMode.Acceleration);
+        if(m_Rigidbody.velocity.magnitude>0 && m_VerticalInput<0)
+        {
+
+        }
+        else if (m_VerticalInput != 0)
         {
             frontLeftWheel.motorTorque = m_VerticalInput * motorForce;
             frontRightWheel.motorTorque = m_VerticalInput * motorForce;
-            backLeftWheel.motorTorque = m_VerticalInput * motorForce * 0.2f;
-            backRightWheel.motorTorque = m_VerticalInput * motorForce * 0.2f;
         }
         else
         {
-            frontLeftWheel.motorTorque = 0f;
-            frontRightWheel.motorTorque = 0f;
-            backLeftWheel.motorTorque = 0f;
-            backRightWheel.motorTorque = 0f;
+            frontLeftWheel.motorTorque = 0;
+            frontRightWheel.motorTorque = 0;
         }
+        //backLeftWheel.motorTorque = m_VerticalInput * motorForce * 0.2f;
+        //backRightWheel.motorTorque = m_VerticalInput * motorForce * 0.2f;
         //print("Gas " + m_VerticalInput);
     }
-
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontRightWheel, frontRightWheelT);
+        UpdateWheelPose(frontLeftWheel, frontLeftWheelT);
+        UpdateWheelPose(backLeftWheel, backLeftWheelT);
+        UpdateWheelPose(backRightWheel, backRightWheelT);
+    }
+    private void UpdateWheelPose(WheelCollider _collider, Transform _transform)
+    {
+        Vector3 _pos = _transform.position;
+        Quaternion _quat = _transform.rotation;
+        _collider.GetWorldPose(out _pos, out _quat);
+    }
     public void FixedUpdate()
     {
         GetInput();
         Accelerate();
         Steer();
+        UpdateWheelPoses();
     }
 }
